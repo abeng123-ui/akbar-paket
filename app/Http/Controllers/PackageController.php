69d8a991c34ebcc6f38494fe8c089b5038f52088
akbarsyidiqi <akbarsyidiq@jojonomic.com>
2020-07-22 09:15:37 +0700

    update package controller

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Traits\PackageTrait;
-use App\Models\Packages;
 use App\Helpers\GlobalHelper;
 
 class PackageController extends Controller
